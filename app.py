26b1d098ee082848e1a3d6a771b14f456ab6f874
Akbarshoh <ismatov2009akb@gmail.com>
2025-05-11 02:10:43 +0500

    bug fix

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_migrate import Migrate
 import os
 from dotenv import load_dotenv
 
@@ -14,7 +13,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+
+# Create database tables before the first request
+with app.app_context():
+    db.create_all()
 
 # Import routes after app and db initialization to avoid circular imports
 from routes.user_routes import *
@@ -23,6 +25,7 @@ from routes.proposal_routes import *
 from routes.contract_routes import *
 from routes.payment_routes import *
 from routes.message_routes import *
+from routes.admin_routes import *
 
-if __name__ == "__app__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
